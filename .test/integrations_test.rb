397191bf22f1a51c19da8c12006856dacd1f42df
Javan Makhmali <javan@javan.us>
2016-08-02 16:57:59 -0700

    Add integrity tests

---
+++ .test/integrations_test.rb
@@ -0,0 +1,51 @@
+require "minitest/autorun"
+require "dimensions"
+require "yaml"
+require "pathname"
+require "uri"
+
+class TestCase < MiniTest::Test
+  ROOT_PATH = Pathname.new(__dir__).join("..")
+  ICON_PATH = ROOT_PATH.join("icons")
+
+  class << self
+    def integrations
+      @integrations ||= YAML.load_file(ROOT_PATH.join("integrations.yml"))
+    end
+
+    def each_integration
+      integrations.each_with_index do |integration, index|
+        name = integration["name"] || "unnamed integration at index #{index}"
+        yield name, integration
+      end
+    end
+  end
+end
+
+class IntegrationsTest < TestCase
+  WEBSITE_PATTERN = /\A#{URI.regexp(%w( http https ))}\z/
+
+  each_integration do |name, integration|
+    define_method "test_#{name.inspect}" do
+      %w( name description website icon ).each do |field|
+        assert present?(integration[field]), "#{field} can't be blank"
+        assert_kind_of String, integration[field], "#{field} must be a string"
+      end
+
+      assert_match WEBSITE_PATTERN, integration["website"], "website must be a valid URL"
+
+      assert (1..8).cover?(integration["category"]), "category must be between 1 and 8"
+      assert_kind_of Integer, integration["category"], "category must be an integer"
+
+      icon_path = ICON_PATH.join(integration["icon"])
+      assert icon_path.exist?, "icon file is missing"
+      assert_equal ".png", icon_path.extname
+      assert_equal [256, 256], Dimensions.dimensions(icon_path), "icon must be 256x256"
+    end
+  end
+
+  private
+    def present?(string = nil)
+      !string.nil? && !string.size.zero?
+    end
+end
